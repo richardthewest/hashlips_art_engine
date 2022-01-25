c46eb02bf17b85eba3771bfb2ee94f5748c759ae
Richard West <r.j.west@live.com.au>
2022-01-25 21:53:01 +1000

    added generic images and metadata to be uplaoded

---
+++ utils/nftport/genericMetas.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const basePath = process.cwd();
+const fs = require("fs");
+const buildDir = path.join(basePath, "/build");
+
+const GENERIC_TITLE = "ScissorsPaperRock" // Replace with what you want the generic titles to say.
+const GENERIC_DESCRIPTION = "Is it Scissors? Is it Paper? Is it Rock? Who knows what it might be!" // Replace with what you want the generic descriptions to say.
+
+if (!fs.existsSync(path.join(buildDir, "/genericJson"))) {
+  fs.mkdirSync(path.join(buildDir, "/genericJson"));
+}
+
+fs.readdirSync(`${buildDir}/json`).forEach((file) => {
+  if (file === "_metadata.json" || file === "_ipfsMetas.json") return;
+
+  const jsonFile = JSON.parse(fs.readFileSync(`${buildDir}/json/${file}`));
+
+  jsonFile.name = `${GENERIC_TITLE} #${jsonFile.custom_fields.edition}`;
+  jsonFile.description = GENERIC_DESCRIPTION;
+  jsonFile.file_url =
+    "https://c.tenor.com/CWgfFh7ozHkAAAAC/rick-astly-rick-rolled.gif";
+    // This is an example url, replace with yours.
+  delete jsonFile.attributes;
+  delete jsonFile.custom_fields.dna;
+
+  fs.writeFileSync(
+    `${buildDir}/genericJson/${file}`,
+    JSON.stringify(jsonFile, null, 2)
+  );
+
+  console.log(`${file} copied and updated!`);
+});
